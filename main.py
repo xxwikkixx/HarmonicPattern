d3e3b3d7cdf9bd7220f8fa1a0231912ae1d5ece0
xxwikkixx <wikkii@msn.com>
2019-09-26 23:06:03 -0400

    Testing features

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
+from timeit import default_timer
 import tosdb
 import time
+from tosdb.intervalize import ohlc
 
-tosdb.init(dllpath= r"C:\TOSDataBridge\bin\Release\x64\tos-databridge-0.9-x64.dll")
+tosdb.init(dllpath=r"C:\TOSDataBridge\bin\Release\x64\tos-databridge-0.9-x64.dll")
 
-# Bool value to check if its connected: True
-# print(tosdb.connected())
+def tosDB():
+    # Bool value to check if its connected: True
+    # print(tosdb.connected())
 
-# Bool value to check if the engine is connected
-# print(tosdb.connection_state()== tosdb.CONN_ENGINE_TOS)
+    # Bool value to check if the engine is connected
+    # print(tosdb.connection_state()== tosdb.CONN_ENGINE_TOS)
 
-block = tosdb.TOSDB_DataBlock(100000, True)
+    block = tosdb.TOSDB_DataBlock(100000, True)
 
-block.add_items('/ES:XCME')
-block.add_topics('bid', 'ask', 'volume')
+    block.add_items('/ES:XCME')
+    block.add_topics('OPEN', 'HIGH', 'LOW', 'bid', 'ask', 'volume')
 
-### NOTICE WE ARE SLEEPING TO ALLOW DATA TO GET INTO BLOCK ###
-print("sleeping for 1.5 seconds")
-time.sleep(1.5)
+    ### NOTICE WE ARE SLEEPING TO ALLOW DATA TO GET INTO BLOCK ###
+    print("sleeping for 1.5 seconds")
+    time.sleep(1.5)
 
-# ['ASK', 'BID', 'VOLUME']
-# print(block.topics())
+    # ['ASK', 'BID', 'VOLUME']
+    # print(block.topics())
 
-print(block.get('/ES:XCME', 'ask'))
-time.sleep(.1)
+    print(block.get('/ES:XCME', 'bid'))
+    time.sleep(.1)
 
-# tosdb.clean_up()
+    tosdb.clean_up()
+
+
+def tosDBohlc():
+    block = ohlc.tosdb.TOSDB_ThreadSafeDataBlock(10000)
+    intrv = ohlc.TOSDB_OpenHighLowCloseIntervals(block, 60)
+    intrv.add_items('/ES:XCME')
+    intrv.add_topics('OPEN', 'HIGH', 'LOW')
+    print(intrv.get('/ES:XCME', 'OPEN'))
+
+    tosdb.clean_up()
+
+
+def test():
+    buffer = []
+    block = tosdb.TOSDB_DataBlock(10000, True)
+    block.add_items('/ES:XCME')
+    block.add_topics('LASTX', 'BIDX', 'ASKX', 'LAST_SIZE')
+    time.sleep(3)  # allow block time to load the cache
+    buffer.append(block.get('/ES:XCME', 'BIDX', date_time=True,
+                            indx=0))  # update marker
+    time.sleep(5)  # wait perhaps for transactions to occur
+    timer = 0
+    while (timer < 5):  # get transactions for 5 seconds
+        data = block.get('/ES:XCME', 'BIDX', date_time=True) # this works
+        # data = block.stream_snapshot_from_marker('/ES:XCME', 'BIDX', date_time=True, beg=0)
+        print(data)  # => prints None
+        buffer.append(data)
+        timer = default_timer()
+    tosdb.clean_up()
+    print(buffer)
+
 
 if __name__ == '__main__':
-    pass
+    test()
